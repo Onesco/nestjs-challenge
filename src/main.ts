c1bde0fccdf95acdb58ae4e499f06eec11affaf5
Onesmus Okonkwo <blessedOnesmus@gmail.com>
2025-05-22 13:43:13 +0100

    debug: check e2e remotly

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { join } from 'path';
 import { AppModule } from './app.module';
 import { AppConfig } from './app.config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // Import Swagger
 import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -22,12 +20,6 @@ async function bootstrap() {
     .setTitle('Record API')
     .setDescription('The record management API')
     .build();
-
-  if (process.env.NODE_ENV !== 'test') {
-    app.useStaticAssets(join(__dirname, '..', 'public'), {
-      index: 'index.html',
-    });
-  }
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
