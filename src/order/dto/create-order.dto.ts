6f526fe66f6a7b44d641921070341131d223203c
Onesmus Okonkwo <blessedOnesmus@gmail.com>
2025-05-27 07:49:54 +0100

    chore: update swagger docs

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,9 +1,21 @@
-import { IsMongoId, IsInt, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsMongoId, IsInt, Min, IsString } from 'class-validator';
 
 export class CreateOrderDto {
+  @ApiProperty({
+    description: 'Record Id',
+    type: String,
+    example: '6834a9fb49c883f8509cc26e',
+  })
   @IsMongoId()
+  @IsString()
   recordId: string;
 
+  @ApiProperty({
+    description: 'Order Quantity',
+    type: Number,
+    example: 4,
+  })
   @IsInt()
   @Min(1)
   qty: number;
